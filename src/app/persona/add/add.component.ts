bf303b2d0b0873bac1af7e260eac051c9757842c
jandroj <103454403+jandroj@users.noreply.github.com>
2023-02-24 11:41:32 -0500

    3 files modified

--- src/app/persona/add/add.component.ts
+++ src/app/persona/add/add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Persona } from '../../modelo/persona';
+import { ServiceService } from '../../service/service.service';
+
 
 @Component({
   selector: 'app-add',
@@ -7,9 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router, private service:ServiceService) { }
 
   ngOnInit() {
   }
-
+  Guardar(persona:Persona){
+    this.service.createPersona(persona)
+    .subscribe(data=>{
+      alert("se agrego usuario");
+      this.router.navigate(["listar"]);
+    })
+  }
 }
